0c3f2a6cf8c1adbe2c4762dcaf9545a9df58fd6d
GeorgiSK <georgiscgd@gmail.com>
2018-02-25 17:21:30 +0200

    Commit some test code

--- index.js
+++ index.js
@@ -5,14 +5,72 @@ const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
     console.log(bot.user.username);
-
-    try{
-        let link = await bot.generateInvite(["ADMINISTRATOR"]);
-        console.log(link);
-    }catch(e){
-        console.log(e.stack);
-    }
-
 });
+
+bot.on("message", async message => {
+    // This event will run on every single message received, from any channel or DM.
+    
+    // It's good practice to ignore other bots. This also makes your bot ignore itself
+    // and not get into a spam loop (we call that "botception").
+    //if(message.author.bot) return;
+    
+    // Also good practice to ignore any message that does not start with our prefix, 
+    // which is set in the configuration file.
+    //if(message.content.indexOf(config.prefix) !== 0) return;
+    
+    // Here we separate our "command" name, and our "arguments" for the command. 
+    // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
+    // command = say
+    // args = ["Is", "this", "the", "real", "life?"]
+    const args = message.content.slice(botSettings.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    
+    // Let's go with a few common example commands! Feel free to delete or change those.
+    
+    if(command === "ping") {
+      // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+      // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+      const m = await message.channel.send("Ping?");
+      m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+    }
+    
+    if(command === "say") {
+      // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+      // To get the "message" itself we join the `args` back into a string with spaces: 
+      const sayMessage = args.join(" ");
+      // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+      message.delete().catch(O_o=>{}); 
+      // And we get the bot to say the thing: 
+      message.channel.send(sayMessage);
+    }
+    
+    if(command === "kick") {
+      // This command must be limited to mods and admins. In this example we just hardcode the role names.
+      // Please read on Array.some() to understand this bit: 
+      // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
+      if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
+        return message.reply("Sorry, you don't have permissions to use this!");
+      
+      // Let's first check if we have a member and if we can kick them!
+      // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
+      let member = message.mentions.members.first();
+      if(!member)
+        return message.reply("Please mention a valid member of this server");
+      if(!member.kickable) 
+        return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+      
+      // slice(1) removes the first part, which here should be the user mention!
+      let reason = args.slice(1).join(' ');
+      if(!reason)
+        return message.reply("Please indicate a reason for the kick!");
+      
+      // Now, time for a swift kick in the nuts!
+      await member.kick(reason)
+        .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+      message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+  
+    }
+});
+
 
 bot.login(botSettings.token);
